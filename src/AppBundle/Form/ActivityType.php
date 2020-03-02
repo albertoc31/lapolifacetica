be0c962de67a62b7a7ee45ae15af5fc32e8fd361
Alberto <albertoc31@gmail.com>
2020-03-02 11:25:49 +0100

    corregir error display datepicker
    No se veia el selector de fecha por un problema de css

--- src/AppBundle/Form/ActivityType.php
+++ src/AppBundle/Form/ActivityType.php
@@ -47,7 +47,7 @@ class ActivityType extends AbstractType
             ->add('foto', FileType::class, ['label' => 'Imagen', 'attr' => ['class' => 'form-control', 'onchange' => 'onChange(event)', 'oldFoto' => $options['oldFoto']], "data_class" => null, 'required' => $options['requireFoto']])
             ->add('asociaciones', EntityType::class, ['class' => 'AppBundle:Asociacion', 'multiple' => true, 'attr' => ['class' => 'form-control']])
             ->add('fechaIni', DateTimeType::class, ['widget' => 'single_text', 'html5' => false, 'attr' => ['class' => 'js-datepicker form-control']])
-            ->add('fechaFin', DateTimeType::class)
+            ->add('fechaFin', DateTimeType::class, ['widget' => 'single_text', 'html5' => false, 'attr' => ['class' => 'js-datepicker form-control']])
             ->add('destacado', CheckboxType::class, ['required' => false])
             ->add('submit', SubmitType::class, ['label' => $options['submitLabel']])
         ;
