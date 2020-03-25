ca5b1775423c8a1f8ad76efeb7f8bcdff44d2942
Alberto <albertoc31@gmail.com>
2020-03-25 12:44:29 +0100

    Administracion: seccion radio 1.1

---
+++ src/AppBundle/Entity/Programa.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Programa
+ *
+ * @ORM\Table(name="programa")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ProgramaRepository")
+ */
+class Programa
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fecha", type="datetime")
+     */
+    private $fecha;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=255)
+     */
+    private $url;
+
+    /**
+     * @ORM\Column(name="colectivo_id", type="integer")
+     * @ORM\ManyToOne(targetEntity="Colectivo", inversedBy="programas")
+     * @ORM\JoinColumn(name="colectivo_id", referencedColumnName="id")
+     */
+    private $colectivo;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Programa
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Programa
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set fecha
+     *
+     * @param \DateTime $fecha
+     *
+     * @return Programa
+     */
+    public function setFecha($fecha)
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+
+    /**
+     * Get fecha
+     *
+     * @return \DateTime
+     */
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     *
+     * @return Programa
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set colectivo
+     *
+     * @param string $colectivo
+     *
+     * @return Programa
+     */
+    public function setColectivo($colectivo)
+    {
+        $this->colectivo = $colectivo;
+
+        return $this;
+    }
+
+    /**
+     * Get colectivo
+     *
+     * @return string
+     */
+    public function getColectivo()
+    {
+        return $this->colectivo;
+    }
+}
+
